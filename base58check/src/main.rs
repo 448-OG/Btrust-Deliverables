063bb2eb6bb96a91a908651bfcc40c6c5c575c3d
448 OG <superuser@448.africa>
2024-02-15 10:47:00 +0300

    feat(article2): Finish code to prepare bytes for conversion to base58
    fix(article2): Fix bugs where wrong base58 values were generated
    
    fix(article2): Fix bugs where wrong bytes were being generated from the base48 string

--- base58check/src/main.rs
+++ base58check/src/main.rs
@@ -1,168 +1,156 @@
-use std::str::FromStr;
-
-use hex_literal::hex;
+use bitcoin::base58;
+use rand_chacha::ChaCha20Rng;
+use rand_core::{RngCore, SeedableRng};
 use sha2::{Digest, Sha256};
+use std::collections::VecDeque;
+
 fn main() {
-    let hashed = Base58Ops::sha256(b"foobar");
-    let hashed_hex = hex::encode(&hashed);
-    dbg!(&hashed_hex);
+    let private_key = Entropy::<4>::generate().0;
+    let mut bytes = Base58Check::new()
+        .add_prefix(&[0u8, 0, 0, 0])
+        .add_payload(&private_key)
+        .calc_checksum()
+        .build();
+    dbg!(base58::encode(&bytes).to_string());
+    let custom_conversion = to_base58(&mut bytes);
+    dbg!(&custom_conversion);
 
-    let mut foo = b"Cat".to_vec();
-    let mut foo = b"28a".to_vec();
-    let mut foo = hashed.to_vec();
-    foo.reverse();
+    println!("{:?}", base58::decode(&base58::encode(&bytes)));
 
-    let mut add_outcome = 0u64;
-
-    for (index, value) in foo.iter().enumerate() {
-        let mul_index = index * 8;
-        let index_pow = 2u8.pow(mul_index as u32) as u64;
-        let outcome = index_pow * *value as u64;
-
-        add_outcome += outcome;
-    }
-
-    dbg!(&add_outcome);
-    let mut values = Vec::<u64>::new();
-
-    while add_outcome != 0 {
-        let (quotient, remainder) = (add_outcome / 58, add_outcome % 58);
-
-        add_outcome = quotient;
-
-        values.push(remainder);
-    }
-
-    values.reverse();
-
-    let base58_string = values
-        .into_iter()
-        .map(|value| BASE58_ALPHABET[value as usize])
-        .collect::<String>();
-
-    dbg!(&base58_string);
-}
-const BASE58_ALPHABET: [char; 58] = [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
-    'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e',
-    'f', 'g', 'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-    'z',
-];
-
-pub struct Base58Ops {
-    data: Vec<u8>,
-    version: Base58VersionByte,
-    checksum: [u8; 4],
+    let to_custom_vec = from_base58(&custom_conversion);
+    assert_eq!(
+        to_custom_vec,
+        base58::decode(&base58::encode(&bytes)).unwrap()
+    );
 }
 
-impl Base58Ops {
-    pub fn new(data: &[u8]) -> Self {
-        Self {
-            data: data.to_vec(),
-            version: Base58VersionByte::default(),
-            checksum: [0u8; 4],
-        }
+#[derive(Debug, Default)]
+pub struct Base58Check {
+    prefix: Vec<u8>,
+    payload: Vec<u8>,
+    checksum: Vec<u8>,
+}
+
+impl Base58Check {
+    fn new() -> Self {
+        Self::default()
     }
 
-    fn checksum(&mut self) -> &mut Self {
-        let mut concat = Vec::<u8>::new();
-        concat.extend_from_slice(self.version.as_bytes());
-
-        let mut first_hash = Base58Ops::sha256(&concat);
-
-        let mut second = Base58Ops::sha256(&first_hash);
-
-        let mut checksum = [0u8; 4];
-        checksum.copy_from_slice(&second.as_slice()[0..=3]);
-
-        self.checksum = checksum;
+    fn add_prefix(mut self, prefix: &[u8]) -> Self {
+        self.prefix.extend_from_slice(prefix);
 
         self
     }
 
-    pub fn sha256(bytes: &[u8]) -> [u8; 32] {
+    fn add_payload(mut self, payload: &[u8]) -> Self {
+        self.payload.extend_from_slice(payload);
+
+        self
+    }
+
+    fn calc_checksum(mut self) -> Self {
         let mut hasher = Sha256::new();
-        hasher.update(&bytes);
+        hasher.update(&self.prefix);
+        hasher.update(&self.payload);
 
-        hasher.finalize().into()
+        let first_hash = hasher.finalize();
+
+        let mut hasher = Sha256::new();
+        hasher.update(first_hash.as_slice());
+        let double_hash = hasher.finalize();
+
+        self.checksum.extend_from_slice(&double_hash[0..4]);
+
+        self
     }
 
-    fn to_hash_160(&self) {}
-}
+    fn build(mut self) -> Vec<u8> {
+        let mut outcome = Vec::<u8>::new();
+        outcome.extend(self.prefix.drain(..));
+        outcome.extend(self.payload.drain(..));
+        outcome.extend(self.checksum.drain(..));
 
-#[derive(Debug, Default)]
-pub enum Base58VersionByte {
-    #[default]
-    BitcoinP2PKH,
-    BitcoinP2SH,
-    TestnetP2PKH,
-    TestnetP2SH,
-    WifPrivateKey,
-    Bip32ExtendedPublicKey,
-}
-
-impl Base58VersionByte {
-    pub const fn as_bytes(&self) -> &[u8] {
-        match self {
-            Self::BitcoinP2PKH => [0u8].as_slice(),
-            Self::BitcoinP2SH => [5u8].as_slice(),
-            Self::TestnetP2PKH => [111u8].as_slice(),
-            Self::TestnetP2SH => [196u8].as_slice(),
-            Self::WifPrivateKey => [128u8].as_slice(),
-            Self::Bip32ExtendedPublicKey => [4u8, 136, 178, 30].as_slice(),
-        }
-    }
-
-    pub const fn to_hex(&self) -> &str {
-        match self {
-            Self::BitcoinP2PKH => "00",
-            Self::BitcoinP2SH => "05",
-            Self::TestnetP2PKH => "6F",
-            Self::TestnetP2SH => "C4",
-            Self::WifPrivateKey => "80",
-            Self::Bip32ExtendedPublicKey => "0488B21E",
-        }
-    }
-
-    pub const fn from_bytes(value: &[u8]) -> Self {
-        match value {
-            &[0u8] => Self::BitcoinP2PKH,
-            &[5u8] => Self::BitcoinP2SH,
-            &[111u8] => Self::TestnetP2PKH,
-            &[196u8] => Self::TestnetP2SH,
-            &[128u8] => Self::WifPrivateKey,
-            &[4u8, 136, 178, 30] => Self::Bip32ExtendedPublicKey,
-            _ => panic!("This can be returned as an error"),
-        }
+        outcome
     }
 }
 
-#[cfg(test)]
-mod prefix_sanity_checks {
-    use crate::Base58VersionByte;
-    use hex_literal::hex;
-    #[test]
-    fn run_test() {
-        assert_eq!(Base58VersionByte::BitcoinP2PKH.as_bytes(), hex!("00"));
-        assert_eq!(Base58VersionByte::BitcoinP2SH.as_bytes(), hex!("05"));
-        assert_eq!(Base58VersionByte::TestnetP2PKH.as_bytes(), hex!("6F"));
-        assert_eq!(Base58VersionByte::TestnetP2SH.as_bytes(), hex!("C4"));
-        assert_eq!(Base58VersionByte::WifPrivateKey.as_bytes(), hex!("80"));
-        assert_eq!(
-            Base58VersionByte::Bip32ExtendedPublicKey.as_bytes(),
-            hex!("0488B21E")
-        );
+fn to_base58(base58_bytes: &mut [u8]) -> String {
+    let mut base58_char = VecDeque::<char>::new();
+    let mut outcome = String::new();
+
+    for _ in base58_bytes.iter().take_while(|&&x| x == 0) {
+        outcome.push('1');
     }
+
+    let mut decimal = 0usize;
+
+    for value in base58_bytes {
+        decimal = (decimal << 8) | *value as usize;
+    }
+
+    let index_alphabet = ALPHABET.chars().collect::<Vec<char>>();
+
+    while decimal != 0 {
+        let (quotient, remainder) = (decimal / 58, decimal % 58);
+
+        base58_char.push_front(index_alphabet[remainder as usize]);
+        decimal = quotient;
+    }
+
+    outcome += base58_char.iter().collect::<String>().as_str();
+
+    outcome
 }
 
-pub struct Mapping;
+fn from_base58(base58_str: &str) -> Vec<u8> {
+    let mut decimal = 0usize;
+    let index_alphabet: Vec<_> = ALPHABET.chars().collect();
 
-impl Mapping {
-    pub fn to_character(value: u8) -> char {
-        if value > 57 {
-            panic!("DECIMAL VALUES CANNOT BE GREATER THAN 57")
-        }
+    let mut leading_zeros_total = 0usize;
+    let mut outcome = Vec::<u8>::new();
+    let mut split_chars = base58_str.chars().collect::<Vec<char>>();
 
-        BASE58_ALPHABET[value as usize]
+    for _ in split_chars.iter().take_while(|&x| x == &'1') {
+        leading_zeros_total += 1;
+        outcome.push(0);
+    }
+
+    split_chars.drain(0..leading_zeros_total);
+
+    for current_char in split_chars {
+        let value = index_alphabet
+            .iter()
+            .position(|&in_alphabet| in_alphabet == current_char)
+            .unwrap();
+        decimal = (decimal * 58) + value;
+    }
+
+    let mut bytes = Vec::<u8>::new();
+
+    while decimal > 0 {
+        let (quotient, remainder) = (decimal / 256, decimal % 256);
+        bytes.push(remainder as u8);
+        decimal = quotient;
+    }
+
+    bytes.reverse();
+
+    outcome.extend_from_slice(&bytes);
+
+    outcome
+}
+
+const ALPHABET: &str = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Entropy<const N: usize>([u8; N]);
+
+impl<const N: usize> Entropy<N> {
+    pub fn generate() -> Self {
+        let mut rng = ChaCha20Rng::from_entropy();
+        let mut buffer = [0u8; N];
+        rng.fill_bytes(&mut buffer);
+
+        Self(buffer)
     }
 }
